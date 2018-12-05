79221b1e02d241a95273d3aaaab067166a3b5d60
Johannes-Knippel <32730055+Johannes-Knippel@users.noreply.github.com>
2018-12-06 00:21:28 +0100

    added different angular components and routet them to app.component.html

--- chat/AdminClient/src/app/app.module.ts
+++ chat/AdminClient/src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from "./user/user.component";
+import { HttpClientModule } from "@angular/common/http";
+import { CountDBComponent } from './count-db/count-db.component';
+import { TraceDBComponent } from './trace-db/trace-db.component';
+import { AdminClHomeComponent } from './admin-cl-home/admin-cl-home.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    CountDBComponent,
+    TraceDBComponent,
+    AdminClHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
